8c08dc239031a068ae627174903bee52f3279195
hpoggi <54382354+hpoggi@users.noreply.github.com>
2023-12-30 00:34:26 +0000

    Add other options

--- .config/nvim/lua/user/vim-options.lua
+++ .config/nvim/lua/user/vim-options.lua
@@ -1,4 +1,45 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+local opt = vim.opt
+
+--tab / indentation
+opt.expandtab = true
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
+
+
+--search
+opt.incsearch = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = false
+
+--appearance
+opt.relativenumber = true
+opt.termguicolors = true
+opt.colorcolumn = '100'
+opt.signcolumn = "yes"
+opt.cmdheight = 1
+opt.scrolloff = 10
+opt.completeopt = "menuone,noinsert,noselect"
+
+--behaviour
+opt.backspace = "indent,eol,start"
+opt.splitright = true
+opt.splitbelow = true
+opt.autochdir = false
+--opt.iskeyword:append("-")
+opt.mouse:append('a')
+opt.clipboard:append("unnamedplus")
+opt.encoding = "UTF-8"
+
+
+--folds
+opt.foldmethod = "expr"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
+opt.foldlevel = 99
+
+
+--vim.cmd("set expandtab")
+--vim.cmd("set tabstop=2")
+--vim.cmd("set softtabstop=2")
+--vim.cmd("set shiftwidth=2")

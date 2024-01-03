f690fa00b0d0828684baa95180b18a5ab6543fc0
Hector Poggi <54382354+hpoggi@users.noreply.github.com>
2024-01-02 23:44:57 -0600

    Update diffview.lua

--- .config/nvim/lua/plugins/diffview.lua
+++ .config/nvim/lua/plugins/diffview.lua
@@ -2,8 +2,24 @@ local M = {
   "sindrets/diffview.nvim",
   event = "VeryLazy",
   cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
+  keys = {
+            --{ "<C-g>", "<CMD>DiffviewOpen<CR>", mode = { "n", "i", "v" } }
+            { "<leader>gd", "<CMD>DiffviewOpen<CR>", mode = { "n", "i", "v" } }
+        },
+
 }
 
 function M.config() end
+ keymaps = {
+                view = {
+                    ["<C-g>"] = "<CMD>DiffviewClose<CR>",
+                    --["c"] = "<CMD>DiffviewClose|Neogit commit<CR>",
+                },
+                file_panel = {
+                    ["<C-g>"] = "<CMD>DiffviewClose<CR>",
+                    --["c"] = "<CMD>DiffviewClose|Neogit commit<CR>",
+                },
+                use_icons = false,
+            },
 
 return M

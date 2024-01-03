473371a50521ed804d3ce5b1fd159fce67325bc4
Hector Poggi <54382354+hpoggi@users.noreply.github.com>
2024-01-02 23:49:21 -0600

    Update treesitter.lua

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -12,6 +12,59 @@ return {
       indent = {
         enable = true
       },
+        incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = 'gnn',
+                        node_incremental = 'grn',
+                        scope_incremental = 'grm',
+                    },
+                },
+                textobjects = {
+                    select = {
+
+                        enable = true,
+                        lookahead = true,
+                        keymaps = {
+                            ['af'] = '@function.outer',
+                            ['if'] = '@function.inner',
+                            ['ac'] = '@class.outer',
+                            ['ic'] = '@class.inner',
+                        },
+                    },
+                    move = {
+
+                        enable = true,
+                        set_jumps = true,
+
+                        goto_next_start = {
+                            [']m'] = '@function.outer',
+                            [']]'] = '@class.outer',
+                        },
+                        goto_next_end = {
+                            [']M'] = '@function.outer',
+                            [']['] = '@class.outer',
+                        },
+                        goto_previous_start = {
+                            ['[m'] = '@function.outer',
+                            ['[['] = '@class.outer',
+                        },
+                        goto_previous_end = {
+                            ['[M]'] = '@function.outer',
+                            ['[]'] = '@class.outer',
+                        },
+
+                    },
+
+                },
+                        matchup = {
+                    enable = true
+                },
+                autotag = {
+                    enable = true,
+                }
+
+
     })
   end
 }

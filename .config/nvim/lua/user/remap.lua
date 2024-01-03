1d1dfb48077054cd8ccb791965b40fb732f6d22a
Hector Poggi <54382354+hpoggi@users.noreply.github.com>
2024-01-02 23:39:54 -0600

    Update remap.lua

--- .config/nvim/lua/user/remap.lua
+++ .config/nvim/lua/user/remap.lua
@@ -1,3 +1,13 @@
 
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+local key_mapper = function(mode, key, result)
+	vim.api.nvim_set_keymap(
+		mode,
+		key,
+		result,
+		{ noremap = true, silent = true }
+	)
+end
+key_mapper('i', 'jk', '<ESC>')
+key_mapper('v', 'jk', '<ESC>')

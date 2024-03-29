dbc74da188739d949807329e46414d7add4baabe
hpoggi <54382354+hpoggi@users.noreply.github.com>
2024-03-29 13:20:18 -0600

    WSL clipboard

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,16 @@
-
+vim.print('init')
 require("user")
+-- This is NeoVim's recommended way to solve clipboard sharing if you use WSL
+--   -- See: https://github.com/neovim/neovim/wiki/FAQ#how-to-use-the-windows-clipboard-from-wsl
+vim.g.clipboard = {
+	name = 'WslClipboard',
+	copy = {
+		['+'] = 'clip.exe',
+		['*'] = 'clip.exe',
+	},
+	paste = {
+		['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+		['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+	},
+	cache_enabled = 0,
+}

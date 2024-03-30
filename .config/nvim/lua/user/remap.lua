d6b44faf77348a29f2cee6071c61dd09eb607765
hpoggi <54382354+hpoggi@users.noreply.github.com>
2024-03-30 16:28:08 +0000

    Add thePrimagen best remaps

--- .config/nvim/lua/user/remap.lua
+++ .config/nvim/lua/user/remap.lua
@@ -1,16 +1,42 @@
-vim.print('Haloo2')
 vim.g.mapleader = " "
+
+-- greatest remap ever
+ vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- next greatest remap ever : asbjornHaland
+ vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+ vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+ vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+
+
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
---local key_mapper = function(mode, key, result)
---	vim.api.nvim_set_keymap(
---		mode,
---		key,
---		result,
---		{ noremap = true, silent = true }
---	)
---end
---key_mapper('i', 'jk', '<ESC>')
---key_mapper('v', 'jk', '<ESC>')
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+local key_mapper = function(mode, key, result)
+	vim.api.nvim_set_keymap(
+		mode,
+		key,
+		result,
+		{ noremap = true, silent = true }
+	)
+end
+key_mapper('i', 'jk', '<ESC>')
+key_mapper('v', 'jk', '<ESC>')
+
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 --key_mapper('n', 'cD', function()
 --	vim.cmd.lcd'%:p:h'
 --	local dir=vim.fs.dirname(vim.fs.find('.git',{upward=true})[1])
